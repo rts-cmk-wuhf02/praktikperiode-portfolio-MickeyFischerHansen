77c6bc87ce4172fc7b78257ed091cb8ee48ce284
Mickey Fischer Hansen <4335582@RTS-365.dk>
2020-06-12 13:25:28 +0200

    skills

--- dist/assets/javascript/index.js
+++ dist/assets/javascript/index.js
@@ -3,3 +3,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Hej verden!");
 });
+
+function myFunction() {
+  var x = document.getElementById("nav");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
